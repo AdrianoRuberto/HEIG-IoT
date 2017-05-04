7ff70d98b53fd231cfda6ae88375b35f85062832
Adriano Ruberto <adriano.ruberto@heig-vd.ch>
2017-05-04 11:57:22 +0200

    Add api/inout

--- HEIG-IoT.py
+++ HEIG-IoT.py
@@ -73,5 +73,25 @@ def parktime(id):
 
     return answer("All data are well formated! Processing data for vehicule " + id + "...")
 
+
+@app.route('/api/inout', methods=['GET'], defaults={'id': None})
+@app.route('/api/inout/<id>', methods=['GET'])
+def inout(id):
+    data = request.args
+
+    print("Received data!\n" + pp.pformat(data))
+    if not all(key in data for key in ("from", "to", "parking")):
+        return answer("Not all key are there! Abort...", 400)
+    if not is_int(data['parking']):
+        return answer("Parking is not numeric!", 400)
+    if not is_int(data['from']):
+        return answer("From is not numeric!", 400)
+    if not is_int(data['to']):
+        return answer("To is not numeric!", 400)
+    if id is None or id == "":
+        return answer("ID is not here, getting for all vehicule!", 400)
+
+    return answer("All data are well formated! Processing data for vehicule " + id + "...")
+
 if __name__ == '__main__':
     app.run(debug=True)
