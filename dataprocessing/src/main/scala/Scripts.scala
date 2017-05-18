a5534b88310ad203e15410db8fe19c5f82df118a
Adriano Ruberto <adriano.ruberto@heig-vd.ch>
2017-05-18 12:54:09 +0200

    Initial structure for processing datas in scala

--- dataprocessing/src/main/scala/Scripts.scala
+++ dataprocessing/src/main/scala/Scripts.scala
@@ -1,8 +1,22 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.rdd.CassandraTableScanRDD //Loads implicit functions
 
-object Scripts {
+object Scripts extends App {
+
+	val keyspaceName = "data"
+
+	val conf: SparkConf = new SparkConf(true)
+		.set("spark.cassandra.connection.host", "127.0.0.1:9042")
+		.set("spark.cassandra.auth.username", "cassandra")
+		.set("spark.cassandra.auth.password", "cassandra")
+
+	val sc = new SparkContext("spark://192.168.123.10:7077", "test", conf)
 
 	// Lis table event et compute
-	def computeDelta(from: Int, to: Int) = ???
+	def computeDelta(from: Int, to: Int) = {
+		val events: CassandraTableScanRDD[CassandraRow] = sc.cassandraTable(keyspaceName, "events")
+		events.where("timestamp > ? AND timestamp < ?", from, to)
+	}
 
 }
